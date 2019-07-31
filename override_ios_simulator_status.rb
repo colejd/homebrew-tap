d68092446f5c7ed8a99d3b666016cc2320dc3731
Jonathan Cole <joncole04412@gmail.com>
2019-07-31 17:17:35 -0400

    Fix incorrect URL

--- override_ios_simulator_status.rb
+++ override_ios_simulator_status.rb
@@ -2,7 +2,7 @@ class OverrideIosSimulatorStatus < Formula
 
   desc "A command line tool for overriding the status bar state on all open iOS simulators."
   homepage "https://github.com/colejd/Override-iOS-Simulator-Status"
-  url "https://github.com/colejd/Override-iOS-Simulator-Status/raw/master/override_ios_simulator_status-1.0.0.tar.gz"
+  url "https://github.com/colejd/Override-iOS-Simulator-Status/raw/master/bin/override_ios_simulator_status-1.0.0.tar.gz"
   sha256 "b616fb38575e344c2f322d391283493c1e313e22057b4237e520fadcc7a50126"
   version "1.0.0" 
 
