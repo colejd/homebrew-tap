2af3ad90d0d82cb84540bafda8658bc36e8a60ef
Jonathan Cole <joncole04412@gmail.com>
2020-01-29 11:59:09 -0500

    Update for new Override-iOS-Simulator-Status 1.1.0

--- override_ios_simulator_status.rb
+++ override_ios_simulator_status.rb
@@ -2,9 +2,9 @@ class OverrideIosSimulatorStatus < Formula
 
   desc "A command line tool for overriding the status bar state on all open iOS simulators."
   homepage "https://github.com/colejd/Override-iOS-Simulator-Status"
-  url "https://github.com/colejd/Override-iOS-Simulator-Status/raw/master/override_ios_simulator_status-1.0.1.tar.gz"
-  sha256 "fa12290565c2a6c884d3610b0deaf484bbc46d3c0f28bb527ef30e7b97e7ae9f"
-  version "1.0.1" 
+  url "https://github.com/colejd/Override-iOS-Simulator-Status/raw/master/override_ios_simulator_status-1.1.0.tar.gz"
+  sha256 "67041ef0920d2d414bb4c0b0af28afdea982ae82450801f18471426425f52d2b"
+  version "1.1.0" 
 
   def install
     bin.install "override_ios_simulator_status"
